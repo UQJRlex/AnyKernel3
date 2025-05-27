01435fdc0c6e199267d61124df1edafe8074d3f7
UQJRlex <xuzhuyin527@gmail.com>
2025-05-27 15:56:57 +0800

    anykernel.sh: edit string

--- anykernel.sh
+++ anykernel.sh
@@ -7,7 +7,7 @@ properties() { '
 kernel.string=LineageOS-22.2+violet+KernelSU
 do.devicecheck=1
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=violet
